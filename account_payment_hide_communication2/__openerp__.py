f3085a163fb4b236a1a46275c1fa6bc69de6b2e6
Alexis de Lattre <alexis@via.ecp.fr>
2014-03-22 01:48:30 +0100

    Add module account_payment_hide_communication2.

---
+++ account_payment_hide_communication2/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Account Payment Hide Communication2 module for OpenERP
+#    Copyright (C) 2014 Akretion (http://www.akretion.com)
+#    @author Alexis de Lattre <alexis.delattre@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Account Payment Hide Communication2',
+    'version': '1.0',
+    'category': 'Accounting & Finance',
+    'license': 'AGPL-3',
+    'summary': 'Hide the field Communication2 on Payment Lines',
+    'description': """
+Account Payment Hide Communication2
+===================================
+
+This module hides the field 'Communication2' on the form view of Payment Lines. I consider that is field is useless and tend to confuse users.
+
+Please contact Alexis de Lattre from Akretion <alexis.delattre@akretion.com> for any help or question about this module.
+""",
+    'author': 'Akretion',
+    'depends': ['account_payment'],
+    'data': [
+        'payment_view.xml',
+        ],
+    'active': False,
+}
