7cd8d59fba509b6d574e41c7ccd76754de097cc9
Alexis de Lattre <alexis@via.ecp.fr>
2014-10-16 12:19:11 +0200

    Add module base_company_extension

---
+++ base_company_extension/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Base Company Extension module for OpenERP/Odoo
+#    Copyright (C) 2014 Akretion (http://www.akretion.com)
+#    @author Alexis de Lattre <alexis.delattre@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Base Company Extension',
+    'version': '0.1',
+    'category': 'Partner',
+    'license': 'AGPL-3',
+    'summary': 'Adds capital and title on company',
+    'description': """
+Base Company Extension
+======================
+
+This module adds a field *Capital Amount* on the Company form, and adds a related field for title.
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': ['base'],
+    'data': ['company_view.xml'],
+    'installable': True,
+}
