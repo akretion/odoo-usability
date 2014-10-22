9657f1bcfd47b57aa97b9c6e766209d09a00ac3d
Alexis de Lattre <alexis@via.ecp.fr>
2014-10-22 18:22:25 +0200

    Add module eradicate_quick_create

---
+++ eradicate_quick_create/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Eradicate Quick Create module for OpenERP/Odoo
+#    Copyright (C) 2014 Akretion (http://www.akretion.com)
+#    @author Alexis de Lattre <alexis.delattre@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Eradicate Quick Create',
+    'version': '0.1',
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'summary': 'Disable quick create on all objects',
+    'description': """
+Eradicate Quick Create
+======================
+
+Disable quick create on all objects of Odoo.
+
+Please contact Alexis de Lattre from Akretion <alexis.delattre@akretion.com> for any help or question about this module.
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': ['base_optional_quick_create'],
+    'data': [],
+}
