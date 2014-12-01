f17ca54c77fa19f96c6448a61b74bbbf43634c09
Alexis de Lattre <alexis@via.ecp.fr>
2014-12-01 15:13:01 +0100

    Add module stock_picking_package_link

---
+++ stock_picking_package_link/stock.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Stock Picking Package Link module for Odoo
+#    Copyright (C) 2014 Akretion (http://www.akretion.com)
+#    @author Alexis de Lattre <alexis.delattre@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api, _
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.one
+    @api.depends('pack_operation_ids', 'pack_operation_ids.result_package_id')
+    def _get_packages(self):
+        package_ids = []
+        for operation in self.pack_operation_ids:
+            if operation.result_package_id:
+                package_ids.append(operation.result_package_id.id)
+        print "packages=", package_ids
+        self.package_ids = package_ids
+
+    package_ids = fields.One2many(
+        'stock.quant.package', compute='_get_packages',
+        string="Packages", readonly=True)
