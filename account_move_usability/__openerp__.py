eba9a4bce5962e1dded73b4b21d4ab491164512d
Alexis de Lattre <alexis@via.ecp.fr>
2015-02-05 15:15:49 +0100

    Rename module account_move_view_more_lines to account_move_usability

--- account_move_usability/__openerp__.py
+++ account_move_usability/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    Account Move View More Lines module for Odoo
+#    Account Move Usability module for Odoo
 #    Copyright (C) 2015 Akretion (http://www.akretion.com)
 #    @author Alexis de Lattre <alexis.delattre@akretion.com>
 #
@@ -19,3 +19,27 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
+
+{
+    'name': 'Account Move Usability',
+    'version': '0.1',
+    'category': 'Accounting & Finance',
+    'license': 'AGPL-3',
+    'summary': 'Small usability enhancements in account move and account move lines',
+    'description': """
+Account Move Usability
+======================
+
+The usability enhancements include:
+* Increase the default limit of 80 lines in account move and account move line view.
+* Fast search on *Reconcile Ref* for account move line.
+
+This module has been written by Alexis de Lattre from Akretion <alexis.delattre@akretion.com>.
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': ['account'],
+    'data': ['account_view.xml'],
+    'installable': True,
+}
