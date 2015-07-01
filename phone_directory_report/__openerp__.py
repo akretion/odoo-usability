5c95d9bd884e231896e31b6be63e02ed732a4def
Alexis de Lattre <alexis@via.ecp.fr>
2015-07-01 10:29:47 +0200

    Add module phone_directory_report

---
+++ phone_directory_report/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Phone Directory Report module for Odoo
+#    Copyright (C) 2015 Akretion (http://www.akretion.com)
+#    @author Alexis de Lattre <alexis.delattre@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Phone Directory',
+    'version': '0.1',
+    'category': 'Phone',
+    'license': 'AGPL-3',
+    'summary': 'Adds a report with the phone directory of the company',
+    'description': """
+Phone Directory Report
+======================
+
+This module has been written by Alexis de Lattre from Akretion <alexis.delattre@akretion.com>.
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': ['asterisk_click2dial', 'report_aeroo'],
+    'data': ['report.xml'],
+    'installable': True,
+}
