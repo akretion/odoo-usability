32e2ecea375a0ab1a4f68964961cddf67d6d37b1
Florian da Costa <florian.dacosta@akretion.com>
2015-10-30 11:52:22 +0100

    add product_search_supplier_code

---
+++ product_search_supplier_code/__openerp__.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Stock Display Sale ID module for OpenERP
+#    Copyright (C) 2015 Akretion (http://www.akretion.com)
+#    @author Florian da Costa <florian.dacosta@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Product Search Supplier Code',
+    'version': '0.1',
+    'category': 'Inventory, Logistic, Storage',
+    'license': 'AGPL-3',
+    'summary': "Allow to search product by its suppliers'code",
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': ['product'],
+    'data': ['product_view.xml'],
+    'installable': True,
+    'active': False,
+}
