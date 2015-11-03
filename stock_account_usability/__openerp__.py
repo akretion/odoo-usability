613d6d688d701efea7dbb58999e61d5bb695f0cb
Alexis de Lattre <alexis.delattre@akretion.com>
2015-11-03 13:26:21 +0100

    Add stock_account_usability module

---
+++ stock_account_usability/__openerp__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Stock Account Usability module for Odoo
+#    Copyright (C) 2015 Akretion (http://www.akretion.com)
+#    @author Alexis de Lattre <alexis.delattre@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Stock Account Usability',
+    'version': '0.1',
+    'category': 'Inventory, Logistic, Storage',
+    'license': 'AGPL-3',
+    'summary': 'Several usability enhancements in stock_account',
+    'description': """
+Stock Account Usability
+========================
+
+The usability enhancements inclure:
+* tracking on the invoice_state field
+
+This module has been written by Alexis de Lattre from Akretion <alexis.delattre@akretion.com>.
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': ['stock_account'],
+    'data': [],
+    'installable': True,
+}
