db55e148c3320c923e1cf28e0656c41e214dfb07
Alexis de Lattre <alexis.delattre@akretion.com>
2016-01-30 00:05:04 +0100

    Better module desc

--- partner_aged_open_invoices/__openerp__.py
+++ partner_aged_open_invoices/__openerp__.py
@@ -1,8 +1,8 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    Partner Aged Open Invoices module for Odoo
-#    Copyright (C) 2015 Akretion (http://www.akretion.com)
+#    Copyright (C) 2015-2016 Akretion (http://www.akretion.com)
 #    @author Alexis de Lattre <alexis.delattre@akretion.com>
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -31,9 +31,9 @@
 Partner Aged Open Invoices
 ==========================
 
-This module adds a button on the partner form view (the icon on the button is a banknote) to easily open the aged open invoices report of this partner.
+This module adds a button on the partner form view (the icon on the button is a banknote) to easily open the aged open invoices report of the partner.
 
-It requires the updated version of the account_financial_report_webkit module that has the aged open invoice report ; you can find in this pull request https://github.com/OCA/account-financial-reporting/pull/102
+It requires the updated version of the module **account_financial_report_webkit** that has the aged open invoice report ; you can find it in this pull request https://github.com/OCA/account-financial-reporting/pull/102
 
 This module has been written by Alexis de Lattre from Akretion <alexis.delattre@akretion.com>.
     """,
