53f85a6b8ea25525d7661697138e5dcec404bfff
David Beal <david.beal@akretion.com>
2016-02-04 17:49:20 +0100

    [ADD] module partner_search

---
+++ partner_search/__openerp__.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# © 2016 David BEAL @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Partner Search',
+    'version': '0.8',
+    'category': 'Base',
+    'summary': "Search view improved",
+    'description': """
+Search view improved: filter on postal code, city, mail, phone, etc...
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': [
+        'base',
+        ],
+    'data': [
+        'partner_view.xml',
+    ],
+    'installable': True,
+}
