b0110ca5422ad0b4cc076c4141cb10cc92b9cb00
David Beal <david.beal@akretion.com>
2016-03-16 17:37:08 +0100

    [ADD] module attribute_usability

---
+++ attribute_usability/__openerp__.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+# © 2016 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Attribute Usability',
+    'version': '8.0.0.0.0',
+    'category': 'Product',
+    'summary': "Attribute views improved",
+    'description': """
+Add a form view to Attribute and Attribute Value views
+
+Contributors: David BEAL
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': [
+        'product',
+        ],
+    'data': [
+        'view.xml',
+    ],
+    'installable': True,
+}
