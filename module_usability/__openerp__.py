d90bd9dd8e4433509b0b03e77c126f1b2ac7db79
David Beal <david.beal@akretion.com>
2016-03-16 09:35:39 +0100

    [ADD] module_usability

---
+++ module_usability/__openerp__.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+# © 2016 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Module Usability',
+    'version': '0.8',
+    'category': 'Base',
+    'summary': "Module views improved",
+    'description': """
+Remove 'Application' filter, add group by 'State'
+
+Why this module ?
+
+- We don't install Applications modules directly but install lower level modules first
+- We must take care of modules state: it's a really precious information
+
+Contributors: David BEAL
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': [
+        'base',
+        ],
+    'data': [
+        'view.xml',
+    ],
+    'installable': True,
+}
