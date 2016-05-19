16627ba6f08ba638fc91ee7fae2b86565b39ddb4
chafique-delli <chafique.delli@akretion.com>
2016-05-19 11:13:52 +0200

    new api for fields_view_get method in purchase_hide_report_print_menu module

--- purchase_hide_report_print_menu/purchase.py
+++ purchase_hide_report_print_menu/purchase.py
@@ -2,28 +2,26 @@
 # © 2016 Chafique DELLI @ Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models
+from openerp import models, api
 
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    def fields_view_get(self, cr, uid, view_id=None, view_type='form',
-                        context=None, toolbar=False, submenu=False):
-        if context is None:
-            context = {}
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form', toolbar=False,
+                        submenu=False):
+        context = self._context
         res = super(PurchaseOrder, self).fields_view_get(
-            cr, uid, view_id=view_id, view_type=view_type, context=context,
-            toolbar=toolbar, submenu=submenu)
+            view_id=view_id, view_type=view_type, toolbar=toolbar,
+            submenu=submenu)
         if ('purchase_order' in context and 'toolbar' in res and
                 'print' in res['toolbar']):
-            model_data_obj = self.pool['ir.model.data']
-            report_purchase_quotation_id = model_data_obj.xmlid_to_res_id(
-                cr, uid, 'purchase.report_purchase_quotation',
-                raise_if_not_found=True)
+            report_purchase_quotation = self.env.ref(
+                'purchase.report_purchase_quotation')
             list_print_submenu_to_hide = []
             for print_submenu in res['toolbar']['print']:
-                if print_submenu['id'] in [report_purchase_quotation_id]:
+                if print_submenu['id'] in [report_purchase_quotation.id]:
                     list_print_submenu_to_hide.append(print_submenu)
             for print_submenu_to_hide in list_print_submenu_to_hide:
                 res['toolbar']['print'].remove(print_submenu_to_hide)
