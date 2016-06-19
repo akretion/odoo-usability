e134153a40ab70bdc5ec512028063f2fe92e0202
David Beal <david.beal@akretion.com>
2016-06-19 10:45:12 +0200

    Update __openerp__.py

--- base_usability/__openerp__.py
+++ base_usability/__openerp__.py
@@ -39,7 +39,7 @@ By default, users in the Partner Contact group also have create/write access on 
 
 It also adds a log message at INFO level when sending an email via SMTP.
 
-It displays the Local modules by default in tree view (instead of Kanban) without filter.
+It displays the local modules by default in tree view (instead of Kanban) without filter.
 A group by 'State' is added to module search view.
     """,
     'author': 'Akretion',
