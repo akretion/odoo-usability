3b32dc25affee789c77c6edde78cb561b96180c3
Benoit <benoit.guillot@akretion.com.br>
2016-06-23 15:50:43 +0200

    [ADD] mail_usability module for improvements on mails

---
+++ mail_usability/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Mail Usability module for Odoo
+#    Copyright (C) 2016 Akretion (http://www.akretion.com)
+#    @author Benoît Guillot <benoit.guillot@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Mail Usability',
+    'version': '0.1',
+    'category': 'Base',
+    'license': 'AGPL-3',
+    'summary': 'Usability improvements on mails',
+    'description': """
+Mail Usability
+==============
+
+Small usability improvements on mails:
+
+* remove link in mail footer
+
+* remove 'sent by' in notification footer
+
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': ['mail'],
+    'data': [],
+    'installable': True,
+}
