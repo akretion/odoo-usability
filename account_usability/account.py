b90bda2aab8cb1d14967e9ad76a29e41814c54c7
Alexis de Lattre <alexis.delattre@akretion.com>
2016-09-14 18:25:01 +0200

    Handle the case where name of account_move_line has the name field of the invoice

--- account_usability/account.py
+++ account_usability/account.py
@@ -101,8 +101,10 @@ class AccountInvoice(models.Model):
         for inv in self:
             if inv.type in ('out_invoice', 'out_refund'):
                 self._cr.execute(
-                    "UPDATE account_move_line SET name=%s WHERE "
-                    "move_id=%s AND name='/'", (inv.number, inv.move_id.id))
+                    "UPDATE account_move_line SET name= "
+                    "CASE WHEN name='/' THEN %s "
+                    "ELSE %s||' - '||name END "
+                    "WHERE move_id=%s", (inv.number, inv.number, inv.move_id.id))
                 self.invalidate_cache()
         return res
 
