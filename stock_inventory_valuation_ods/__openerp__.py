b2bf902a782ed6c18e1e03283b00fc4a9bbebaf3
Alexis de Lattre <alexis.delattre@akretion.com>
2016-10-11 15:32:30 +0200

    Set all modules as uninstallable

--- stock_inventory_valuation_ods/__openerp__.py
+++ stock_inventory_valuation_ods/__openerp__.py
@@ -39,5 +39,5 @@ This module has been written by Alexis de Lattre from Akretion <alexis.delattre@
     'website': 'http://www.akretion.com',
     'depends': ['stock_inventory_valuation', 'report_aeroo'],
     'data': ['report.xml'],
-    'installable': True,
+    'installable': False,
 }
