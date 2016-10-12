b8615215710d38bdccfd7f6a14d29f67b6d374d7
Alexis de Lattre <alexis.delattre@akretion.com>
2016-10-12 22:52:20 +0200

    Port account_bank_statement_import_usability to v10

--- account_bank_statement_import_usability/__manifest__.py
+++ account_bank_statement_import_usability/__manifest__.py
@@ -1,25 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Account Bank Statement Import Usability module for Odoo
-#    Copyright (C) 2015 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# © 2015-2016 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Bank Statement Import Usability',
@@ -33,11 +15,9 @@ Account Bank Statement Import Usability
 
 This module adds the following changes:
 
-* Blocks the *Automagically create bank account*, because it's too dangerous : it creates new bank accounts and new account journal... and the user doesn't even realize that !
-
 * Works if the bank statement file only contain the account number and not the full IBAN
 
-* If you have 2 accounts with the same number (I know a company that has an account in EUR and an account in USD with the same number), you should force the journal and it will work (instead of blocking with an error message)
+* remove start balance and end balance (doesn't work with OFX, which is one of the most used file format !)
 
 This module has been written by Alexis de Lattre from Akretion <alexis.delattre@akretion.com>.
     """,
@@ -45,5 +25,5 @@ This module has been written by Alexis de Lattre from Akretion <alexis.delattre@
     'website': 'http://www.akretion.com',
     'depends': ['account_bank_statement_import'],
     'data': ['account_view.xml'],
-    'installable': False,
+    'installable': True,
 }
--- account_bank_statement_import_usability/account_bank_statement_import.py
+++ account_bank_statement_import_usability/account_bank_statement_import.py
@@ -1,64 +1,19 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Account Bank Statement Import Usability module for Odoo
-#    Copyright (C) 2015 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015-2016 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, api
+from odoo import models, api
 
 
 class AccountBankStatementImport(models.TransientModel):
     """Extend model account.bank.statement."""
     _inherit = 'account.bank.statement.import'
 
-    @api.model
-    def _find_bank_account_id(self, account_number):
-        """Compared to the code in the module account_bank_statement_import,
-        this code:
-        - works when the account_number is not a complete IBAN,
-          but just an account number (most statement files only have the
-          account number)
-        - works if you have 2 bank accounts with the same number
-          (I have seen that at Crédit du Nord: the company had 1 account in USD
-          and 1 account in EUR with the same number !)
-          -> for that, I filter on the journal if the journal_id field is set
-          """
-        bank_account_id = None
-        if account_number and len(account_number) > 4:
-            if self.journal_id:
-                self._cr.execute("""
-                    SELECT id FROM res_partner_bank
-                    WHERE replace(replace(acc_number,' ',''),'-','') like %s
-                    AND journal_id=%s
-                    ORDER BY id
-                    """, ('%' + account_number + '%', self.journal_id.id))
-            else:
-                self._cr.execute("""
-                    SELECT id FROM res_partner_bank
-                    WHERE replace(replace(acc_number,' ',''),'-','') like %s
-                    AND journal_id is not null
-                    ORDER BY id
-                    """, ('%' + account_number + '%', ))
-            bank_account_ids = [id[0] for id in self._cr.fetchall()]
-            if bank_account_ids:
-                bank_account_id = bank_account_ids[0]
-        return bank_account_id
+    def _check_journal_bank_account(self, journal, account_number):
+        if account_number in journal.bank_account_id.sanitized_acc_number:
+            return True
+        return False
 
 
 class AccountBankStatement(models.Model):
@@ -76,5 +31,6 @@ class AccountBankStatement(models.Model):
     # This code is present in the 'account' module, but I override it here
     # and not in account_usability because the users who don't have
     # account_bank_statement_import may want to keep start/end balance
-    def balance_check(self, cr, uid, st_id, journal_type='bank', context=None):
+    @api.multi
+    def _balance_check(self):
         return True
