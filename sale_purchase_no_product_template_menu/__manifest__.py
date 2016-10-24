96a7e6e44325603055b8460746b2a02e816a9420
Alexis de Lattre <alexis.delattre@akretion.com>
2016-10-24 15:33:18 +0200

    Rapid (partial ?) port of product_manager_group and sale_purchase_no_product_template_menu to v10
    pos_no_product_template_menu is useless in v10 -> remove it

--- sale_purchase_no_product_template_menu/__manifest__.py
+++ sale_purchase_no_product_template_menu/__manifest__.py
@@ -1,25 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Sale Purchase No Product Template Menu module for Odoo
-#    Copyright (C) 2015 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# © 2015-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Sale Purchase No Product Template Menu',
@@ -39,7 +20,11 @@ This module has been written by Alexis de Lattre from Akretion <alexis.delattre@
     """,
     'author': 'Akretion',
     'website': 'http://www.akretion.com',
-    'depends': ['purchase', 'sale', 'base_usability'],
+    'depends': [
+        'purchase',
+        'sale',
+        #'base_usability',
+        ],
     'data': ['view.xml'],
-    'installable': False,
+    'installable': True,
 }
