1a46f397055d93dff58b998a9d2a5a860370a597
Alexis de Lattre <alexis.delattre@akretion.com>
2016-10-25 12:42:05 +0200

    Port mrp_usability and pos_usability to v10

--- pos_usability/__manifest__.py
+++ pos_usability/__manifest__.py
@@ -1,25 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    POS Usability module for Odoo
-#    Copyright (C) 2014-2015 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# © 2014-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Point of Sale Usability',
@@ -31,9 +12,7 @@
 Point of Sale Usability
 =======================
 
-Small usability enhancements in POS:
-
-* Add a filter *Available in POS* in Product search view
+Minor usability enhancements in POS:
 
 This module has been written by Alexis de Lattre from Akretion <alexis.delattre@akretion.com>.
     """,
@@ -41,5 +20,5 @@ This module has been written by Alexis de Lattre from Akretion <alexis.delattre@
     'website': 'http://www.akretion.com',
     'depends': ['point_of_sale'],
     'data': ['pos_view.xml'],
-    'installable': False,
+    'installable': True,
 }
