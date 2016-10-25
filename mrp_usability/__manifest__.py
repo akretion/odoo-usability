55b9b1a6195b0050501d76341b43d73383c9071d
Alexis de Lattre <alexis.delattre@akretion.com>
2016-10-25 12:42:05 +0200

    Port mrp_usability and pos_usability to v10

--- mrp_usability/__manifest__.py
+++ mrp_usability/__manifest__.py
@@ -1,24 +1,7 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    MRP Usability module for Odoo
-#    Copyright (C) 2015 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2015-2016 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
 {
@@ -35,7 +18,11 @@ Small usability improvements on MRP:
 
 * order by id desc
 
-* show date start and date finished on production order form view
+* show field date_start and date_finished on mrp.production form view
+
+* show more fields on stock move form
+
+* show bom type in tree view + add group by
 
 This module has been written by Alexis de Lattre from Akretion <alexis.delattre@akretion.com>.
     """,
@@ -43,5 +30,5 @@ This module has been written by Alexis de Lattre from Akretion <alexis.delattre@
     'website': 'http://www.akretion.com',
     'depends': ['mrp'],
     'data': ['mrp_view.xml'],
-    'installable': False,
+    'installable': True,
 }
