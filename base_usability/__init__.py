6d887f79cf471a8ffabf5071ec83c911d9b96c29
Alexis de Lattre <alexis.delattre@akretion.com>
2016-10-27 09:33:15 +0200

    Port base_usability, sale_usability, sale_stock_usability to v10

--- base_usability/__init__.py
+++ base_usability/__init__.py
@@ -3,4 +3,3 @@
 from . import partner
 from . import mail
 from . import misc
-from . import ir_sequence
--- base_usability/__manifest__.py
+++ base_usability/__manifest__.py
@@ -1,25 +1,7 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Base Usability module for Odoo
-#    Copyright (C) 2014-2015 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# -*- coding: utf-8 -*-
+# © 2014-2016 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Base Usability',
@@ -44,14 +26,13 @@ A group by 'State' is added to module search view.
     """,
     'author': 'Akretion',
     'website': 'http://www.akretion.com',
-    'depends': ['base', 'mail'],
+    'depends': ['base'],
     'data': [
         'security/group.xml',
         'security/ir.model.access.csv',
         'partner_view.xml',
         'country_view.xml',
         'module_view.xml',
-        'translation_view.xml',
         ],
-    'installable': False,
+    'installable': True,
 }
