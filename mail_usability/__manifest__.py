1a8588ea24c3ab4a7b42774db7858b6c26d5cde8
Alexis de Lattre <alexis.delattre@akretion.com>
2016-12-02 19:44:56 +0100

    Port mail_usability to v10

--- mail_usability/__manifest__.py
+++ mail_usability/__manifest__.py
@@ -1,29 +1,12 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Mail Usability module for Odoo
-#    Copyright (C) 2016 Akretion (http://www.akretion.com)
-#    @author Benoît Guillot <benoit.guillot@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# © 2016 Akretion (http://www.akretion.com)
+# @author Benoît Guillot <benoit.guillot@akretion.com>
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Mail Usability',
-    'version': '0.1',
+    'version': '10.0.1.0.0',
     'category': 'Base',
     'license': 'AGPL-3',
     'summary': 'Usability improvements on mails',
@@ -41,6 +24,6 @@ Small usability improvements on mails:
     'author': 'Akretion',
     'website': 'http://www.akretion.com',
     'depends': ['mail'],
-    'data': [],
-    'installable': False,
+    'data': ['mail_view.xml'],
+    'installable': True,
 }
