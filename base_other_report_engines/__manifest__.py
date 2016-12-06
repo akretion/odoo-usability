3eab0c9d7a62fe45b6e134dafe8f99cb75675cf7
Alexis de Lattre <alexis.delattre@akretion.com>
2016-12-06 09:28:47 +0100

    Port base_other_report_engines to v10

--- base_other_report_engines/__manifest__.py
+++ base_other_report_engines/__manifest__.py
@@ -1,30 +1,12 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Base Other Report Engines module for Odoo
-#    Copyright (C) 2014-2015 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# © 2014-2016 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Base Other Report Engines',
-    'version': '0.1',
-    'category': '',
+    'version': '10.0.1.0.0',
+    'category': 'Hidden',
     'license': 'AGPL-3',
     'summary': 'Allows the use of report engines other than Qweb',
     'description': """
@@ -36,5 +18,5 @@ This module inherit the method *_get_report_from_name()* to allow the use of rep
     'author': 'Akretion',
     'website': 'http://www.akretion.com',
     'depends': ['report'],
-    'data': [],
+    'installable': True,
 }
--- base_other_report_engines/report.py
+++ base_other_report_engines/report.py
@@ -1,34 +1,19 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Base Other Report Engines module for Odoo
-#    Copyright (C) 2014-2015 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2014-2016 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models
+from odoo import models
 
 
 class Report(models.Model):
     _inherit = "report"
 
-    def _get_report_from_name(self, cr, uid, report_name):
-        """Remove condition ('report_type', 'in', qwebtypes)"""
-        report_obj = self.pool['ir.actions.report.xml']
+    def _get_report_from_name(self, report_name):
+        """Remove condition ('report_type', 'in', qwebtypes)
+        This code is copyright Odoo S.A.
+        """
+        report_obj = self.env['ir.actions.report.xml']
         conditions = [('report_name', '=', report_name)]
-        idreport = report_obj.search(cr, uid, conditions)[0]
-        return report_obj.browse(cr, uid, idreport)
+        context = self.env['res.users'].context_get()
+        return report_obj.with_context(context).search(conditions, limit=1)
