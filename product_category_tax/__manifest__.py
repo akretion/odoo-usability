42b35059663737b01b8bd1935b7e378e93d9d22b
Alexis de Lattre <alexis.delattre@akretion.com>
2016-12-07 10:08:37 +0100

    Port product_category_tax to v10

--- product_category_tax/__manifest__.py
+++ product_category_tax/__manifest__.py
@@ -1,29 +1,11 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Product Category Tax module for Odoo
-#    Copyright (C) 2015 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# -*- coding: utf-8 -*-
+# © 2015-2016 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Product Category Tax',
-    'version': '0.1',
+    'version': '10.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Adds sale and purchase taxes on product category',
@@ -32,6 +14,5 @@
     'website': 'http://www.akretion.com',
     'depends': ['account'],
     'data': ['product_view.xml'],
-    'demo': [],
-    'installable': False,
+    'installable': True,
 }
