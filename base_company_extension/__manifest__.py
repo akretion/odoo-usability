94fd30c4b3d98e7670ed248580ed373e140ebdea
Alexis de Lattre <alexis.delattre@akretion.com>
2016-12-15 11:13:26 +0100

    Move  account_fiscal_position_translate in account_usability
    Update code conventions in base_company_extension

--- base_company_extension/__manifest__.py
+++ base_company_extension/__manifest__.py
@@ -1,29 +1,11 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Base Company Extension module for Odoo
-#    Copyright (C) 2014-2015 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# © 2014-2016 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Base Company Extension',
-    'version': '0.1',
+    'version': '10.0.1.0.0',
     'category': 'Partner',
     'license': 'AGPL-3',
     'summary': 'Adds capital and title on company',
