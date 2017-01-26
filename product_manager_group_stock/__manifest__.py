ac84bf6468fbdd4493fdeeb4179da3f178d58ee2
Alexis de Lattre <alexis.delattre@akretion.com>
2017-01-26 10:44:44 +0100

    Port product_manager_group_stock to v10

--- product_manager_group_stock/__manifest__.py
+++ product_manager_group_stock/__manifest__.py
@@ -1,29 +1,10 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Product Manager Group Stock module for OpenERP
-#    Copyright (C) 2014 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# -*- coding: utf-8 -*-
+# © 2014-2017 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
 
 {
     'name': 'Product Manager Group Stock',
-    'version': '0.1',
+    'version': '10.0.1.0.0',
     'category': 'Hidden',
     'license': 'AGPL-3',
     'summary': 'Extend the group Product Manager to Stock',
@@ -33,7 +14,7 @@ Product Manager Group Stock
 
 Extends the group Product Manager to Stock Management.
 
-Please contact Alexis de Lattre from Akretion <alexis.delattre@akretion.com> for any help or question about this module.
+This module has been written by Alexis de Lattre from Akretion <alexis.delattre@akretion.com>.
     """,
     'author': 'Akretion',
     'website': 'http://www.akretion.com',
@@ -41,5 +22,6 @@ Please contact Alexis de Lattre from Akretion <alexis.delattre@akretion.com> for
     'data': [
         'security/ir.model.access.csv',
         ],
-    'active': False,
+    'installable': True,
+    'auto_install': True,
 }
