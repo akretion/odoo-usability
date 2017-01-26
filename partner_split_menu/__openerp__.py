e71ad5f952d046c73023da91dee1f38bfdb7e742
Sébastien BEAU <sebastien.beau@akretion.com>
2017-01-26 14:38:09 +0100

    [IMP] add module partner split menu to avoid the mix of contact and main partner

---
+++ partner_split_menu/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+{
+    "name": "Partner Menu Split",
+    "summary": "Split the customer menu in two",
+    "version": "8.0.0.0",
+    "category": "Uncategorized",
+    "website": "www.akretion.com",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
