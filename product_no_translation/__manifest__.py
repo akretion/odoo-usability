2cbd9dda0a44fa196113fa5a6fafd7519b49d61b
MonsieurB <contact@ebii.fr>
2017-01-31 16:16:18 +0100

    migrate V10

---
+++ product_no_translation/__manifest__.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+# alexis de latte @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Product no Translation',
+    'version': '10.0.1.0.0',
+    'category': 'Sales Management',
+    'license': 'AGPL-3',
+    'summary': 'For companies that work with only one language',
+    'description': """
+        This module sets the translatable fields of the product
+        object (name, descriptions) to non-translatable fields.
+
+This change is usefull for companies that work with only one language.
+And it reduces the start time of the Point of Sale !
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': ['product'],
+}
