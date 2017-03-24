2df224cb0ad80f03e0b296925cf73ad67645d1b0
Alexis de Lattre <alexis.delattre@akretion.com>
2017-03-24 18:01:20 +0100

    Add module base_partner_ref for better use of 'ref' field on partners

---
+++ base_partner_ref/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# © 2017 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Base Partner Reference',
+    'version': '10.0.1.0.0',
+    'category': 'Partner',
+    'license': 'AGPL-3',
+    'summary': "Improve usage of partner's Internal Reference",
+    'description': """
+Base Partner Reference
+======================
+
+* Adds Internal Reference in partner tree view
+
+* Adds Internal Reference in name_get()
+
+* Adds unicity constraint on Internal Reference
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': ['base'],
+    'data': ['partner_view.xml'],
+    'installable': True,
+}
