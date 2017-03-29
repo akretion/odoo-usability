37281620afa280c12b27b07d4e6e045c4092f4a5
Alexis de Lattre <alexis.delattre@akretion.com>
2017-03-29 17:45:16 +0200

    Better log message for outgoing emails

--- base_usability/mail.py
+++ base_usability/mail.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, api
+from odoo.addons.base.ir.ir_mail_server import extract_rfc2822_addresses
 import logging
 
 logger = logging.getLogger(__name__)
@@ -16,11 +17,17 @@ class IrMailServer(models.Model):
             self, message, mail_server_id=None, smtp_server=None,
             smtp_port=None, smtp_user=None, smtp_password=None,
             smtp_encryption=None, smtp_debug=False):
-
-        logger.info("Sending email to %s copy %s with subject %s",
-                    message.get('To'),
-                    message.get('Cc'),
-                    message.get('Subject'))
+        # Start copy from native method
+        smtp_from = message['Return-Path'] or\
+            self._get_default_bounce_address() or message['From']
+        from_rfc2822 = extract_rfc2822_addresses(smtp_from)
+        smtp_from = from_rfc2822[-1]
+        # End copy from native method
+        logger.info(
+            "Sending email from '%s' to '%s' Cc '%s' Bcc '%s' "
+            "with subject '%s'",
+            smtp_from, message.get('To'), message.get('Cc'),
+            message.get('Bcc'), message.get('Subject'))
         return super(IrMailServer, self).send_email(
             message, mail_server_id=mail_server_id,
             smtp_server=smtp_server, smtp_port=smtp_port,
