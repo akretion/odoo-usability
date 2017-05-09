bfd0c6f8cd457588cb279d08939a2b0e2f026c8e
Alexis de Lattre <alexis.delattre@akretion.com>
2017-05-09 17:09:34 +0200

    Add module resource_usability

---
+++ resource_usability/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# © 2017 Akretion (http://www.akretion.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+
+{
+    'name': 'Resource Usability',
+    'version': '8.0.1.0.1',
+    'category': 'Hidden/Dependency',
+    'license': 'AGPL-3',
+    'summary': 'Resource usability enhancements',
+    'description': """
+Resource Usability
+==================
+
+The usability improvements include :
+
+* Add a name_get() on resource.calendar.leaves
+
+This module has been written by Alexis de Lattre from Akretion
+<alexis.delattre@akretion.com>.
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': ['resource'],
+    'data': ['resource_view.xml'],
+    'installable': True,
+}
---
+++ resource_usability/resource.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2017 Akretion (http://www.akretion.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+
+from openerp import models, api
+
+
+class ResourceCalendarLeaves(models.Model):
+    _inherit = "resource.calendar.leaves"
+
+    @api.multi
+    def name_get(self):
+        res = []
+        for caleaves in self:
+            name = caleaves.name
+            if caleaves.resource_id:
+                if name:
+                    name = '%s (%s)' % (name, caleaves.resource_id.name)
+                else:
+                    name = caleaves.resource_id.name
+            res.append((caleaves.id, name))
+        return res
