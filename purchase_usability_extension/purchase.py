d736663f3f8007a4206f38fe437af26783c70680
Alexis de Lattre <alexis.delattre@akretion.com>
2017-05-19 09:47:10 +0200

    Fix view

--- purchase_usability_extension/purchase.py
+++ purchase_usability_extension/purchase.py
@@ -1,6 +1,6 @@
-# coding: utf-8
-#    Copyright (C) 2015 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
+# -*- coding: utf-8 -*-
+# © 2015-2017 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, fields, api
--- purchase_usability_extension/supplierinfo.py
+++ purchase_usability_extension/supplierinfo.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 # © 2015 David BEAL @ Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
