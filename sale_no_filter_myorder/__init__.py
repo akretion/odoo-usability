bb849c02284d3ce5e2153d2083beb98c5ae9ed18
Alexis de Lattre <alexis.delattre@akretion.com>
2017-09-14 18:59:56 +0200

    Remove module sale_no_filter_myorder, which is not useful in v10

--- sale_no_filter_myorder/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2015 Akretion (http://www.akretion.com)
-# Sébastien BEAU <sebastien.beau@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
