a11f9574eebe7f0aa6f6ea61259e1f7f92f73ae3
Alexis de Lattre <alexis.delattre@akretion.com>
2017-11-22 09:52:51 +0100

    Add track_visibility='onchange' on available_in_pos

---
+++ pos_usability/product.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    available_in_pos = fields.Boolean(track_visibility='onchange')
