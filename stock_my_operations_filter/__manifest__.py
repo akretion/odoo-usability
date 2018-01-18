9a8d9616b7fdc779f6a000dd3e48979e9f004063
Alexis de Lattre <alexis.delattre@akretion.com>
2018-01-18 18:08:54 +0100

    Port module stock_my_operations_filter to v10

--- stock_my_operations_filter/__manifest__.py
+++ stock_my_operations_filter/__manifest__.py
@@ -1,29 +1,11 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Stock My Operations Filter module for Odoo
-#    Copyright (C) 2015 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015-2018 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
 
 
 {
     'name': 'Stock My Operations Filter',
-    'version': '8.0.0.1.0',
+    'version': '10.0.0.1.0',
     'category': 'Inventory, Logistic, Storage',
     'license': 'AGPL-3',
     'summary': "Adds a filter 'My Operations'",
@@ -40,5 +22,5 @@ This module has been written by Alexis de Lattre from Akretion
         'stock_view.xml',
         'users_view.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
