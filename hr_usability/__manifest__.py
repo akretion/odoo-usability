d61e48635b4487ff9923856fc59db9dc9ea98d32
Alexis de Lattre <alexis.delattre@akretion.com>
2018-03-01 21:56:40 +0100

    Port hr_usability to v10

--- hr_usability/__manifest__.py
+++ hr_usability/__manifest__.py
@@ -1,29 +1,12 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    HR Usability module for Odoo
-#    Copyright (C) 2015 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright 2015-2018 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
 {
     'name': 'HR Usability',
-    'version': '0.1',
+    'version': '10.0.1.0.0',
     'category': 'Human Resources',
     'license': 'AGPL-3',
     'summary': 'Better usability in HR module',
@@ -41,5 +24,5 @@ This module has been developped by Alexis de Lattre <alexis.delattre@akretion.co
     'data': [
         'hr_view.xml',
         ],
-    'installable': False,
+    'installable': True,
 }
--- hr_usability/hr.py
+++ hr_usability/hr.py
@@ -1,26 +1,10 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    HR Usability module for Odoo
-#    Copyright (C) 2016 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2016-2018 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api
+
+from odoo import models, fields, api
 
 
 class HrEmployee(models.Model):
@@ -31,7 +15,6 @@ class HrEmployee(models.Model):
     ssnid = fields.Char(track_visibility='onchange')
     sinid = fields.Char(track_visibility='onchange')
     identification_id = fields.Char(track_visibility='onchange')
-    otherid = fields.Char(track_visibility='onchange')
     gender = fields.Selection(track_visibility='onchange')
     marital = fields.Selection(track_visibility='onchange')
     department_id = fields.Many2one(track_visibility='onchange')
@@ -44,3 +27,18 @@ class HrEmployee(models.Model):
     coach_id = fields.Many2one(track_visibility='onchange')
     job_id = fields.Many2one(track_visibility='onchange')
     passport_id = fields.Char(track_visibility='onchange')
+
+    _sql_constraints = [
+        ('identification_unique',
+        'unique(identification_id)',
+        'An employee with this Identification No already exists'),
+        ('ssnid_unique',
+        'unique(ssnid)',
+        'An employee with this Social Security Number already exists'),
+        ('sinid_unique',
+        'unique(sinid)',
+        'An employee with this Social Insurance Number already exists'),
+        ('passport_id_unique',
+        'unique(passport_id)',
+        'An employee with this Passport No already exists'),
+        ]
