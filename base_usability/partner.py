b199228e1d8c5fd80c67e186c29098632495379f
Alexis de Lattre <alexis.delattre@akretion.com>
2018-03-22 18:24:52 +0100

    Add string on bank_name of res.partner.bank

--- base_usability/partner.py
+++ base_usability/partner.py
@@ -135,3 +135,12 @@ class ResPartnerCategory(models.Model):
     _inherit = 'res.partner.category'
 
     name = fields.Char(translate=False)
+
+
+class ResPartnerBank(models.Model):
+    _inherit = 'res.partner.bank'
+
+    # In the 'base' module, they didn't put any string, so the bank name is
+    # displayed as 'Name', which the string of the related field it
+    # points to
+    bank_name = fields.Char(string='Bank Name')
