d3bddf5fdaf1b2e72e8e6e3fe15f18cb4a7a62f1
Alexis de Lattre <alexis.delattre@akretion.com>
2018-06-07 16:56:48 +0200

    Port stock_picking_type_default_partner to v10

--- stock_picking_type_default_partner/__manifest__.py
+++ stock_picking_type_default_partner/__manifest__.py
@@ -1,45 +1,26 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Stock Picking Type Default Partner module for Odoo
-#    Copyright (C) 2014 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright (C) 2014-2018 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
 {
     'name': 'Stock Picking Type Default Partner',
-    'version': '0.1',
-    'category': 'Inventory, Logistic, Storage',
+    'version': '10.0.1.0.0',
+    'category': 'Inventory, Logistics, Warehousing',
     'license': 'AGPL-3',
     'summary': 'Adds a default partner on types of operation',
     'description': """
 Stock Picking Type Default Partner
 ==================================
 
-This module adds a new field on the Types of Operation (stock.picking.type) : *Default Partner*.
+This module adds a new field on the Types of Operation (stock.picking.type) : *Default Partner*. This is useful for multi-site companies that create inter-site Type of Operations: all the operations that use this Type of Operation should have the same destination partner.
 
 This module has been written by Alexis de Lattre from Akretion <alexis.delattre@akretion.com>.
     """,
     'author': 'Akretion',
     'website': 'http://www.akretion.com',
     'depends': ['stock'],
-    'data': [
-        'stock_view.xml'
-        ],
-    'installable': False,
+    'data': ['stock_view.xml'],
+    'installable': True,
 }
