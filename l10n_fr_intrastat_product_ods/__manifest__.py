a7a8271a38e055364d40a813c6e06d6df51053e5
Alexis de Lattre <alexis.delattre@akretion.com>
2018-06-22 22:51:28 +0200

    Port l10n_fr_intrastat_product_ods to v10

--- l10n_fr_intrastat_product_ods/__manifest__.py
+++ l10n_fr_intrastat_product_ods/__manifest__.py
@@ -1,43 +1,25 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    L10n FR Intrastat Product ODS module for Odoo
-#    Copyright (C) 2016 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+#  Copyright (C) 2016-2018 Akretion (http://www.akretion.com)
+#  @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'L10n FR Intrastat Product ODS',
-    'version': '8.0.0.1.0',
+    'version': '10.0.0.1.0',
     'category': 'Tools',
     'license': 'AGPL-3',
-    'summary': 'Adds an Aeroo ODS report on DEB',
+    'summary': 'Adds a Py3o ODS report on DEB',
     'description': """
 L10n FR Intrastat Product ODS
 =============================
 
-This module will add an Aeroo ODS report on French Intrastat Product Declaration (DEB). It depends on the new intrastat modules that are currently under review in the following OCA PR: https://github.com/OCA/account-financial-reporting/pull/80 and https://github.com/OCA/l10n-france/pull/50
+This module will add a Py3o ODS report on French Intrastat Product Declaration (DEB).
 
 This module has been written by Alexis de Lattre from Akretion <alexis.delattre@akretion.com>.
     """,
     'author': "Akretion",
     'website': 'http://www.akretion.com',
-    'depends': ['l10n_fr_intrastat_product', 'report_aeroo'],
+    'depends': ['l10n_fr_intrastat_product', 'report_py3o'],
     'data': ['report.xml'],
-    'installable': False,
+    'installable': True,
 }
