ebb8f1ad86bf2e30e1b5e110acec2096f7f55a38
Alexis de Lattre <alexis.delattre@akretion.com>
2018-07-18 14:27:21 +0200

    carrier_id not readonly on done picking (add tracking on it)
    fix for formatLang inherit in base_usability

---
+++ delivery_usability/stock.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    carrier_id = fields.Many2one(track_visibility='onchange')
