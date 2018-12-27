e2b86ff11c75acd2a572a672786f882587987c65
Raphaël Valyi <rvalyi@akretion.com>
2018-12-27 01:54:51 -0200

    [MIG] product_usability: Migration to 12.0

--- product_usability/__manifest__.py
+++ product_usability/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2015-2016 Akretion (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -6,7 +5,7 @@
 
 {
     'name': 'Product Usability',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Product',
     'license': 'AGPL-3',
     'summary': 'Small usability enhancements to the product module',
