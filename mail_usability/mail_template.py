a370c9100c495428acf6109b7647ff867da7159f
Sébastien BEAU <sebastien.beau@akretion.com>
2019-01-08 23:25:42 +0100

    [IMP] remove the fucking auto_delete!!!

---
+++ mail_usability/mail_template.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import fields, models
+
+
+class MailTemplate(models.Model):
+    _inherit = 'mail.template'
+
+    auto_delete = fields.Boolean(default=False)
