5c55ac7f1c25ead94799486e3dcd62ada69edbe5
Alexis de Lattre <alexis.delattre@akretion.com>
2019-05-17 20:27:52 +0200

    New module link_tracker_usability

---
+++ link_tracker_usability/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Link Tracker Usability',
+    'version': '10.0.1.0.0',
+    'category': 'Marketing',
+    'license': 'AGPL-3',
+    'summary': 'Improve usability for link tracker',
+    'description': """
+Link Tracker Usability
+======================
+
+Several small usability improvements.
+
+This module has been written by Alexis de Lattre from Akretion
+<alexis.delattre@akretion.com>.
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': ['link_tracker'],
+    'data': [
+        'link_tracker_view.xml',
+        ],
+    'installable': True,
+}
