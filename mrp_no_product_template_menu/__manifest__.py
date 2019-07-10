60a5fa7a332904b4fed412fd3ab42bb3cb4b8533
Alexis de Lattre <alexis.delattre@akretion.com>
2019-07-10 10:45:14 +0200

    Better display of technical name of module in kanban view
    Small improvements in stock_usability, sale_purchase_no_product_template_menu and mrp_no_product_template_menu

--- mrp_no_product_template_menu/__manifest__.py
+++ mrp_no_product_template_menu/__manifest__.py
@@ -1,4 +1,5 @@
-# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016-2019 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
