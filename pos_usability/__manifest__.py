fc4ea217e014fd87cda79e05e6b00f63a153fbf9
David Beal <david.beal@akretion.com>
2019-10-11 15:12:21 +0200

    ADD module pos_usability

---
+++ pos_usability/__manifest__.py
@@ -0,0 +1,29 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "POS Usability",
+    "version": "12.0.1.0.0",
+    "category": "Point of sale",
+    "license": "AGPL-3",
+    "summary": "Misc usability improvement for point of sale",
+    "description": """
+POS Usability
+=============
+
+- Sales Details report : add sub total amount by tax
+
+
+authors
+-------
+
+Akretion:
+
+* David Béal <david.beal@akretion.com>
+
+    """,
+    "author": "Akretion",
+    "website": "http://www.akretion.com",
+    "depends": ["point_of_sale"],
+    "data": ["report/pos.xml"],
+    "installable": True,
+}
