01cfcbf80d21dc10f5627f795460bfb9dae77ae8
Alexis de Lattre <alexis.delattre@akretion.com>
2019-12-02 11:21:59 +0100

    sale_order_add_bom: fix related field definition

--- sale_order_add_bom/mrp_bom.py
+++ sale_order_add_bom/mrp_bom.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016-2019 Akretion France
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class MrpBom(models.Model):
     _inherit = 'mrp.bom'
     _rec_name = 'product_id'
 
-    sale_ok = fields.Boolean(related='product_id.sale_ok', store=True, compute_sudo=True)
+    sale_ok = fields.Boolean(related='product_tmpl_id.sale_ok', store=True)
