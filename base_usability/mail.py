91353275809af9c4d3681cc0cbe02b7d25e465f1
Florent Cayré <florent@commown.fr>
2019-12-17 12:38:26 +0100

    Improve send email logging by adding attachment names

--- base_usability/mail.py
+++ base_usability/mail.py
@@ -39,11 +39,17 @@ class IrMailServer(models.Model):
         from_rfc2822 = extract_rfc2822_addresses(smtp_from)
         smtp_from = from_rfc2822[-1]
         # End copy from native method
+        attachment_names = [
+            decode(part.get_filename())
+            for part in message.walk()
+            if part.get_content_maintype() not in ('multipart', 'text')]
         logger.info(
             "Sending email from '%s' to '%s' Cc '%s' Bcc '%s' "
-            "with subject '%s'",
+            "with subject '%s' and attachments %s",
             smtp_from, decode(message.get('To')), decode(message.get('Cc')),
-            decode(message.get('Bcc')), decode(message.get('Subject')))
+            decode(message.get('Bcc')), decode(message.get('Subject')),
+            u', '.join([u"'%s'" % n for n in attachment_names])
+        )
         return super(IrMailServer, self).send_email(
             message, mail_server_id=mail_server_id,
             smtp_server=smtp_server, smtp_port=smtp_port,
