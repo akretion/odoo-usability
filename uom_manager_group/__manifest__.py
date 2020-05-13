16424a6897af62ccc5bb505496ed87dc85465882
clementmbr <clement.mombereau@akretion.com.br>
2020-05-13 18:02:04 -0300

    [12.0][MIG] product_unit_manager_group + rename
    Basic module migration and :
    
    - rename module's name product_unit_manager_group into uom_manager_group
    in order to follow the new separation between product and uom modules
    in Odoo 12.0
    - light refactor adding a group category "Units of Measure" with two
    levels :
        - nothing : neither UoM fields nor menus are displayed
        - User : UoM fields are displayed but not Uom menus (Users are not
        allowed to create or modify UoM)
        - Manager : display both fields and UoM menus
    
    The rest of the module philosophy is maintained. All the Sales
    Manager, Stock Manager, MRP Manager have no more rights to create or
    modify UoMs. Only "UoM Managers" can.

---
+++ uom_manager_group/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2017 Chafique DELLI @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Unit of Measure Manager Group",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Akretion",
+    "website": "http://akretion.com",
+    "depends": ["sale", "purchase", "mrp"],
+    "data": [
+        "data/ir_module_category_data.xml",
+        "security/product_security.xml",
+        "security/ir.model.access.csv",
+        "views/product_view.xml",
+    ],
+    "installable": True,
+}
