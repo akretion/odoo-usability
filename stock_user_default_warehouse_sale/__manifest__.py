ba78791ab1529a54efdeb0cfed8eb5275a651486
Daniel Luque <danielluque14@gmail.com>
2020-06-02 15:00:12 +0200

    Adds README to stock_user_default_warehouse_sale

--- stock_user_default_warehouse_sale/__manifest__.py
+++ stock_user_default_warehouse_sale/__manifest__.py
@@ -8,15 +8,6 @@
     'category': 'Sale Management',
     'license': 'AGPL-3',
     'summary': "Use the users's default warehouse on sale orders",
-    'description': """
-Default Warehouse on User (Sale)
-================================
-
-The default warehouse configured in the preferences of the user will be used by default on sale orders.
-
-This module has been written by Alexis de Lattre from Akretion
-<alexis.delattre@akretion.com>.
-    """,
     'author': 'Akretion',
     'website': 'http://www.akretion.com',
     'depends': ['sale_stock', 'stock_user_default_warehouse_base'],
