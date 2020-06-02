cd51a6784f109e662f4f8721421b608e2cb06814
Daniel Luque <danielluque14@gmail.com>
2020-06-02 14:45:36 +0200

    Adds README to stock_user_default_warehouse_purchase

--- stock_user_default_warehouse_purchase/__manifest__.py
+++ stock_user_default_warehouse_purchase/__manifest__.py
@@ -8,15 +8,6 @@
     'category': 'Purchases',
     'license': 'AGPL-3',
     'summary': "Use the users's default warehouse on purchase orders",
-    'description': """
-Default Warehouse on User (Purchase)
-====================================
-
-The default warehouse configured in the preferences of the user will be used by default for the picking type on purchase orders.
-
-This module has been written by Alexis de Lattre from Akretion
-<alexis.delattre@akretion.com>.
-    """,
     'author': 'Akretion',
     'website': 'http://www.akretion.com',
     'depends': ['purchase', 'stock_user_default_warehouse_base'],
