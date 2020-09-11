1074fcba2143e8bbdab136c66c583d04f74eea73
Alexis de Lattre <alexis.delattre@akretion.com>
2020-09-11 14:33:05 +0200

    Show property_cost_method on product form view

--- stock_account_usability/__manifest__.py
+++ stock_account_usability/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2019 Akretion (http://www.akretion.com)
+# Copyright 2019-2020 Akretion France (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -18,6 +18,8 @@ The usability enhancements include:
 
 * activate the refund option by default in return wizard on pickings
 
+* show field *property_cost_method* on product form view
+
 * add ability to select a stock location on the inventory valuation report
 
 
@@ -26,6 +28,9 @@ This module has been written by Alexis de Lattre from Akretion <alexis.delattre@
     'author': 'Akretion',
     'website': 'http://www.akretion.com',
     'depends': ['stock_account'],
-    'data': ['wizard/stock_quantity_history_view.xml'],
+    'data': [
+        'product_view.xml',
+        'wizard/stock_quantity_history_view.xml',
+        ],
     'installable': True,
 }
