7a6600431ca9cd69f06e3fee385f13d9c9732185
Florian da Costa <florian.dacosta@akretion.com>
2020-11-02 11:32:38 +0100

    Migrate account_fiscal_position_payable_receivable to v12

--- account_fiscal_position_payable_receivable/__manifest__.py
+++ account_fiscal_position_payable_receivable/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Fiscal Position Payable Receivable',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Configure payable/receivable accounts on fiscal positions',
@@ -20,7 +19,7 @@ This module has been written by Alexis de Lattre from Akretion <alexis.delattre@
     'website': 'http://www.akretion.com',
     'depends': ['account'],
     'data': [
-        'account_fiscal_position_view.xml',
+        'views/account_fiscal_position_view.xml',
     ],
     'installable': True,
 }
--- account_fiscal_position_payable_receivable/models/res_partner.py
+++ account_fiscal_position_payable_receivable/models/res_partner.py
@@ -1,19 +1,7 @@
-# -*- coding: utf-8 -*-
 # © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
-
-
-class AccountFiscalPosition(models.Model):
-    _inherit = 'account.fiscal.position'
-
-    receivable_account_id = fields.Many2one(
-        'account.account', string='Partner Receivable Account',
-        company_dependent=True, domain=[('internal_type', '=', 'receivable')])
-    payable_account_id = fields.Many2one(
-        'account.account', string='Partner Payable Account',
-        company_dependent=True, domain=[('internal_type', '=', 'payable')])
+from odoo import models, api
 
 
 class ResPartner(models.Model):
