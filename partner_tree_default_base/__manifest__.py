d72872eb90b01ba0d8440861b3e89fb3a7e5466d
Alexis de Lattre <alexis.delattre@akretion.com>
2020-12-03 23:48:14 +0100

    [MIG] partner_tree_default to 3 modules: partner_tree_default_base, partner_tree_default_account and partner_tree_default_contacts

--- partner_tree_default_base/__manifest__.py
+++ partner_tree_default_base/__manifest__.py
@@ -1,26 +1,25 @@
-# -*- coding: utf-8 -*-
-# © 2016 Akretion (http://www.akretion.com)
+# Copyright 2016-2020 Akretion (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
 {
-    'name': 'Partner Tree Default',
-    'version': '12.0.1.0.0',
+    'name': 'Partner Tree Default - Base',
+    'version': '14.0.1.0.0',
     'category': 'Partner',
     'license': 'AGPL-3',
     'summary': 'Tree view by default instead of kanban for partners',
     'description': """
-Partner Tree Default
-====================
+Partner Tree Default - Base
+===========================
 
-With this module, when you select a *Customer* or *Supplier* menu entry, you will see the list view by default instead of the kanban view.
+With this module, when you select a *Customer* or *Vendors* menu entry, you will see the list view by default instead of the kanban view.
 
 This module has been written by Alexis de Lattre <alexis.delattre@akretion.com> from Akretion.
     """,
     'author': 'Akretion',
     'website': 'http://www.akretion.com',
     'depends': ['base'],
-    'data': ['views/partner_view.xml'],
-    'installable': False,
+    'data': ['views/res_partner.xml'],
+    'installable': True,
 }
---
+++ partner_tree_default_contacts/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2016-2020 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+{
+    'name': 'Partner Tree Default - Contacts',
+    'version': '14.0.1.0.0',
+    'category': 'Partner',
+    'license': 'AGPL-3',
+    'summary': 'Tree view by default instead of kanban for partners',
+    'description': """
+Partner Tree Default - Contacts
+===============================
+
+With this module, when you select a *Customer* or *Vendors* menu entry, you will see the list view by default instead of the kanban view.
+
+This module has been written by Alexis de Lattre <alexis.delattre@akretion.com> from Akretion.
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': ['base'],
+    'data': ['views/res_partner.xml'],
+    'installable': True,
+}
