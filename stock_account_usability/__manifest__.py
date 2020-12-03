86ec56b610a833c74ff1cca4bf9f0871a0ba585f
Alexis de Lattre <alexis.delattre@akretion.com>
2020-12-03 23:26:16 +0100

    Remove un-needed code in stock_account_usability
    As the module is now empty, don't make it installable for the moment

--- stock_account_usability/__manifest__.py
+++ stock_account_usability/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019-2020 Akretion France (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -6,7 +5,7 @@
 
 {
     'name': 'Stock Account Usability',
-    'version': '12.0.1.0.0',
+    'version': '14.0.1.0.0',
     'category': 'Hidden',
     'license': 'AGPL-3',
     'summary': 'Several usability enhancements on stock_account',
@@ -16,12 +15,7 @@ Stock Account Usability
 
 The usability enhancements include:
 
-* activate the refund option by default in return wizard on pickings
-
-* show field *property_cost_method* on product form view
-
-* add ability to select a stock location on the inventory valuation report
-
+NONE
 
 This module has been written by Alexis de Lattre from Akretion <alexis.delattre@akretion.com>.
     """,
@@ -29,8 +23,6 @@ This module has been written by Alexis de Lattre from Akretion <alexis.delattre@
     'website': 'http://www.akretion.com',
     'depends': ['stock_account'],
     'data': [
-        'product_view.xml',
-        'wizard/stock_quantity_history_view.xml',
         ],
     'installable': False,
 }
