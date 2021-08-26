b05abba06437fe9397bf5161823650f3d491469b
Alexis de Lattre <alexis.delattre@akretion.com>
2021-08-26 23:18:37 +0200

    [MIG] sale_order_route to v14

--- sale_order_route/__manifest__.py
+++ sale_order_route/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2019 Akretion France (http://www.akretion.com/)
+# Copyright 2019-2021 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Sale Order Route',
-    'version': '12.0.1.0.0',
+    'version': '14.0.1.0.0',
     'category': 'Sales',
     'license': 'AGPL-3',
     'summary': 'Set route on sale order',
@@ -18,5 +18,5 @@ This module has been written by Alexis de Lattre from Akretion
     'website': 'http://www.akretion.com',
     'depends': ['sale_stock'],
     'data': ['views/sale_order.xml'],
-    'installable': False,
+    'installable': True,
 }
--- sale_order_route/models/sale_order.py
+++ sale_order_route/models/sale_order.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Akretion France (http://www.akretion.com/)
+# Copyright 2019-2021 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -10,17 +10,17 @@ class SaleOrder(models.Model):
 
     route_id = fields.Many2one(
         'stock.location.route', string='Route',
-        ondelete='restrict', readonly=True, track_visibility='onchange',
+        ondelete='restrict', readonly=True, tracking=True,
         states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
-        domain=[('sale_selectable', '=', True)])
+        check_company=True,
+        domain="['|', ('company_id', '=', company_id), ('company_id', '=', False), ('sale_selectable', '=', True)]")
 
     def _action_confirm(self):
-        # Caution: take into account the scenario where
-        # route_id has a value, then SO is cancelled+back to draft,
-        # then route_id = False and SO is confirmed again
+        # Takes into account the scenario where route_id has a value, then SO is
+        # cancelled+back to draft, then route_id = False and SO is confirmed again
         for order in self:
             vals = {'route_id': order.route_id.id or False}
             order.order_line.filtered(
                 lambda l:
-                l.product_id.type in ('product', 'consu')).write(vals)
-        super(SaleOrder, self)._action_confirm()
+                l.product_id and l.product_id.type in ('product', 'consu')).write(vals)
+        return super()._action_confirm()
