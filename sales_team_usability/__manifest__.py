5e4d1b3c8d85a559f91539ce5c22aacd858cda87
Alexis de Lattre <alexis.delattre@akretion.com>
2021-11-18 10:43:53 +0100

    [MIG] crm_usability from v10 to v14
    New module sales_team_usability (remove translation on crm.tag, which
    was in crm_usability in v10)

--- sales_team_usability/__manifest__.py
+++ sales_team_usability/__manifest__.py
@@ -1,25 +1,27 @@
-# Copyright 2016-2021 Akretion (http://www.akretion.com)
+# Copyright 2021 Akretion France (http://www.akretion.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 
 {
-    'name': 'CRM Usability',
+    'name': 'Sales Teams Usability',
     'version': '14.0.1.0.0',
-    'category': 'Customer Relationship Management',
+    'category': 'Sales/Sales',
     'license': 'AGPL-3',
-    'summary': 'CRM usability enhancements',
+    'summary': 'Sales Teams usability enhancements',
     'description': """
-CRM Usability
-=============
+Sales Teams Usability
+=====================
+
+The usability improvements include:
+
+* set 'name' field of crm.tag un-translatable
 
 This module has been written by Alexis de Lattre from Akretion
 <alexis.delattre@akretion.com>.
     """,
     'author': 'Akretion',
     'website': 'http://www.akretion.com',
-    'depends': ['crm'],
-    'data': [
-        'views/crm_lead.xml',
-        ],
+    'depends': ['sales_team'],
+    'data': [],
     'installable': True,
 }
