878db1d0a1aa96359be7c8485ab51a5d0262981c
Alexis de Lattre <alexis.delattre@akretion.com>
2021-11-18 10:43:53 +0100

    [MIG] crm_usability from v10 to v14
    New module sales_team_usability (remove translation on crm.tag, which
    was in crm_usability in v10)

---
+++ crm_usability/models/crm_lead.py
@@ -0,0 +1,13 @@
+# Copyright 2017-2021 Akretion (http://www.akretion.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+
+from odoo import fields, models
+
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+
+    probability = fields.Float(tracking=100)
+    date_deadline = fields.Date(tracking=110)
+    name = fields.Char(tracking=1)
