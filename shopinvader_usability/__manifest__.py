df673718e572a0071b2c963c9a4efb2bad5ecf0a
Kevin Khao <kevinkhao@gmail.com>
2021-11-26 18:47:38 +0300

    [14.0][ADD] shopinvader_usability

---
+++ shopinvader_usability/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopinvader Usability",
+    "description": """
+        Shopinvader Usability""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Akretion",
+    "website": "https://github.com/OCA/odoo-usability",
+    "depends": [
+        "shopinvader",
+        "sale_usability",
+    ],
+    "data": ["views/sale_views.xml"],
+    "auto_install": True,
+}
