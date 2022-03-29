f6b10a7caa7e3c3c29f0e349a74f3f8910e91405
Alexis de Lattre <alexis.delattre@akretion.com>
2022-03-29 11:36:59 +0200

    [MIG] sale_order_add_bom from v10 to v14

--- sale_order_add_bom/models/mrp_bom.py
+++ sale_order_add_bom/models/mrp_bom.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016-2019 Akretion France
+# Copyright 2016-2022 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -8,6 +7,5 @@ from odoo import models, fields
 
 class MrpBom(models.Model):
     _inherit = 'mrp.bom'
-#    _rec_name = 'product_id'
 
     sale_ok = fields.Boolean(related='product_tmpl_id.sale_ok', store=True)
--- sale_order_add_bom/wizard/__init__.py
+++ sale_order_add_bom/wizard/__init__.py
@@ -1,3 +1,1 @@
-# -*- coding: utf-8 -*-
-
 from . import sale_add_phantom_bom
