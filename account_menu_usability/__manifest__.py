5fbe6d340a550425bdaf8a19246aa1f03252d3e2
Alexis de Lattre <alexis.delattre@akretion.com>
2022-07-13 22:07:50 +0200

    Add module account_menu_usability

---
+++ account_menu_usability/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Akretion France (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Menu Usability',
+    'version': '14.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'license': 'AGPL-3',
+    'summary': 'Small usability enhancements in account_menu module',
+    'author': 'Akretion',
+    'website': 'https://github.com/akretion/odoo-usability',
+    'depends': ['account_menu'],
+    'data': [
+        'views/account_menu.xml',
+        ],
+    'installable': True,
+}
