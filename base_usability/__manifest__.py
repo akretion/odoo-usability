23222e997bacab05f103cf86413c2f3df2b5ce22
Alexis de Lattre <alexis.delattre@akretion.com>
2022-09-23 17:47:05 +0200

    base_usability: add currency_id in res.company tree view

--- base_usability/__manifest__.py
+++ base_usability/__manifest__.py
@@ -17,6 +17,7 @@
         'views/res_partner.xml',
         'views/res_partner_bank.xml',
         'views/res_country.xml',
+        'views/res_company.xml',
         'views/ir_module.xml',
         'views/ir_sequence.xml',
         'views/ir_property.xml',
