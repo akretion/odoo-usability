688a07fc5e6b0fe502fea3ac56dac3b1d3e30e20
Sébastien BEAU <sebastien.beau@akretion.com>
2022-10-21 16:30:39 +0200

    add sale_show_transaction, that make transaction really visible on sale order

---
+++ sale_show_transaction/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2022 Akretion (https://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Show Transaction",
+    "summary": "Make transaction hyper visible on sale order",
+    "version": "14.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Uncategorized",
+    "website": "www.akretion.com",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_view.xml",
+    ],
+    "demo": [
+    ],
+}
