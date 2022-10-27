8486c75f90e3ef62f22bcc3343587e33925cbc8d
Alexis de Lattre <alexis.delattre@akretion.com>
2022-10-27 23:13:29 +0200

    [MIG] delivery_usability to v16

--- delivery_usability/__manifest__.py
+++ delivery_usability/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2018-2021 Akretion (http://www.akretion.com)
+# Copyright 2018-2022 Akretion (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Delivery Usability',
-    'version': '14.0.1.0.0',
+    'version': '16.0.1.0.0',
     'category': 'Stock',
     'license': 'AGPL-3',
     'summary': 'Several usability enhancements in Delivery',
@@ -23,5 +23,5 @@ This module has been written by Alexis de Lattre from Akretion <alexis.delattre@
     'data': [
         'views/stock_picking.xml',
         ],
-    'installable': False,
+    'installable': True,
 }
