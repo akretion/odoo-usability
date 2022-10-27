71ebe2adea8d309cc566fd423832c5d6dcca4965
Alexis de Lattre <alexis.delattre@akretion.com>
2022-10-27 22:04:17 +0200

    [MIG] purchase_stock_usability to v16

--- purchase_stock_usability/__manifest__.py
+++ purchase_stock_usability/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright (C) 2014-2021 Akretion (http://www.akretion.com)
+# Copyright 2014-2022 Akretion (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Purchase Stock Usability',
-    'version': '14.0.1.0.0',
+    'version': '16.0.1.0.0',
     'category': 'Purchases',
     'license': 'AGPL-3',
     'summary': 'Usability improvements on purchase_stock module',
@@ -14,7 +14,7 @@ Purchase Stock Usability
 
 Several usability improvements on the official purchase_stock module:
 
-Please contact Alexis de Lattre from Akretion <alexis.delattre@akretion.com> for any help or question about this module.
+This module has been written by Alexis de Lattre from Akretion France.
     """,
     'author': 'Akretion',
     'website': 'http://www.akretion.com',
@@ -25,5 +25,5 @@ Please contact Alexis de Lattre from Akretion <alexis.delattre@akretion.com> for
     'data': [
         'views/stock_picking.xml',
         ],
-    'installable': False,
+    'installable': True,
 }
