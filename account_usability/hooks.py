947d287c1333efe88a0328bc9f65bde7e0cb24ca
Raphaël Valyi <rvalyi@akretion.com>
2022-12-06 05:35:36 -0300

    [TEMP] attempt to avoid inconsistent AML writes

--- account_usability/hooks.py
+++ account_usability/hooks.py
@@ -6,4 +6,4 @@ from odoo import SUPERUSER_ID, api
 
 def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    env["account.move.line"].update_matching_number()
+    # env["account.move.line"].update_matching_number()
