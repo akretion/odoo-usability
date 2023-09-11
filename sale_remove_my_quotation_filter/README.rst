48247b6d061d1a6604b009f79f531e132d82771d
Alexis de Lattre <alexis.delattre@akretion.com>
2023-09-11 18:42:08 +0200

    Add module sale_remove_my_quotation_filter

---
+++ sale_remove_my_quotation_filter/README.rst
@@ -0,0 +1,18 @@
+===============================
+Sale Remove My Quotation Filter
+===============================
+
+This module removes the default filter **My Quotations** in the menu entry *Sales > Orders > Quotations*.
+
+Credits
+=======
+
+Authors
+~~~~~~~
+
+* Akretion
+
+Contributors
+~~~~~~~~~~~~
+
+* Alexis de Lattre <alexis.delattre@akretion.com>
---
+++ sale_remove_my_quotation_filter/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Akretion France (https://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Sale remove My Quotations filter',
+    'version': '16.0.1.0.0',
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'summary': 'Remove default filter My Quotations',
+    'author': 'Akretion',
+    'website': 'https://github.com/akretion/odoo-usability',
+    'depends': ['sale'],
+    'data': ['views/sale_order.xml'],
+    'installable': True,
+}
