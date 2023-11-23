b8e335fb105b276d1ad196228768f33a59aa98d5
Alexis de Lattre <alexis.delattre@akretion.com>
2023-11-23 11:51:37 +0100

    [MIG] link_tracker_usability to v16

--- link_tracker_usability/__manifest__.py
+++ link_tracker_usability/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2019-2021 Akretion France (http://www.akretion.com)
+# Copyright 2019-2023 Akretion France (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Link Tracker Usability',
-    'version': '14.0.1.0.0',
+    'version': '16.0.1.0.0',
     'category': 'Marketing',
     'license': 'AGPL-3',
     'summary': 'Improve usability for link tracker',
@@ -18,10 +18,10 @@ This module has been written by Alexis de Lattre from Akretion
 <alexis.delattre@akretion.com>.
     """,
     'author': 'Akretion',
-    'website': 'http://www.akretion.com',
+    'website': 'https://github.com/akretion/odoo-usability',
     'depends': ['link_tracker'],
     'data': [
         'views/link_tracker_click.xml',
         ],
-    'installable': False,
+    'installable': True,
 }
