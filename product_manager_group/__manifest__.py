a4c2a43cf700ba7aace4bcaa67709edbf196e0dd
Olivier Nibart <olivier@naya-tec.com>
2023-12-11 11:10:26 +0100

    [16.0][MIG] product_manager_group

--- product_manager_group/__manifest__.py
+++ product_manager_group/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2014-2020 Akretion (http://www.akretion.com/)
+# Copyright 2014-2023 Akretion (http://www.akretion.com/)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Product Manager Group',
-    'version': '14.0.1.0.0',
+    'version': '16.0.1.0.0',
     'category': 'Hidden',
     'license': 'AGPL-3',
     'summary': 'Add a group Product Manager',
@@ -23,5 +23,5 @@ This module has been written by Alexis de Lattre from Akretion <alexis.delattre@
         'security/product_security.xml',
         'security/ir.model.access.csv',
         ],
-    'installable': False,
+    'installable': True,
 }
