0b7d595b30d0f110efb46968cb6e96684c31dc6f
Olivier Nibart <olivier@naya-tec.com>
2023-12-11 11:11:26 +0100

    [16.0][MIG] product_manager_group_stock

--- product_manager_group_stock/__manifest__.py
+++ product_manager_group_stock/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2014-2020 Akretion (http://www.akretion.com)
+# Copyright 2014-2023 Akretion (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Product Manager Group Stock',
-    'version': '14.0.1.0.0',
+    'version': '16.0.1.0.0',
     'category': 'Hidden',
     'license': 'AGPL-3',
     'summary': 'Extend the group Product Manager to Stock',
@@ -22,6 +22,6 @@ This module has been written by Alexis de Lattre from Akretion <alexis.delattre@
     'data': [
         'security/ir.model.access.csv',
         ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
 }
