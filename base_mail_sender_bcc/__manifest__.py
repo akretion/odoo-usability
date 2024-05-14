9507be2943798dbf6bfd2edf9ee07bf74d9ce30f
Arnaud LAYEC <arnaudlayec@gmail.com>
2024-05-14 22:00:21 +0200

    [base_mail_sender_bcc] manifest version change from 14 to 16.0.1.0.0

--- base_mail_sender_bcc/__manifest__.py
+++ base_mail_sender_bcc/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Mail Sender Bcc',
-    'version': '14.0.1.0.0',
+    'version': '16.0.1.0.0',
     'category': 'Mail',
     'license': 'AGPL-3',
     'summary': "Always send a copy of the mail to the sender",
