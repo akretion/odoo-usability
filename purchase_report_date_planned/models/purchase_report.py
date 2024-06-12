c5309423446c8fea18715215d32b6de9c8375f82
Mathieu <mathieu.delva@hotmail.com>
2024-06-12 15:36:02 +0200

    [UPD] remove compute function

--- purchase_report_date_planned/models/purchase_report.py
+++ purchase_report_date_planned/models/purchase_report.py
@@ -4,14 +4,7 @@ from odoo import fields, models
 class PurchaseReport(models.Model):
     _inherit = "purchase.report"
 
-    date_planned = fields.Datetime(compute="_compute_date_planned", store=True)
-
-    def _compute_date_planned(self):
-        for record in self:
-            order_line = record.order_id.order_line.filtered(
-                lambda r: r.product_id == record.product_id
-            )
-            record.date_planned = order_line.date_planned
+    date_planned = fields.Datetime(store=True)
 
     def _select(self):
         select_str = super(PurchaseReport, self)._select()
